9bfe5fa1f01f2ef17359d2a2174144df2d1fcb73
Parodix007 <sebastian.siarczynski@gmail.com>
2020-11-06 19:45:55 +0100

    Unowoczesnienie zapisu aplikacji

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require('express')
-const bodyparser = require('body-parser')
+
 const app = express()
 const port = 8080
+
+app.use(express.json())
+app.use(express.urlencoded({extended=true}))
 
 app.get('/', (req, res) => res.send('cos tam'))
 app.get('/id/:x', (req, res) => {
